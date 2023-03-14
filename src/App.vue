158cd1ce80644ea53f4670136722141d43d82c78
kurokaj <jomikuro@gmail.com>
2023-03-14 21:30:05 +0200

    Small changes. Have to make this work as Vue app..

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,30 @@
 <script setup>
-  import * as THREE from "three";
-  import { onMounted } from "vue";
-  
-  // Scene
-  const scene = new THREE.Scene();
-  
-  // Object
-  const geometry = new THREE.TorusGeometry( 10, 3, 16, 10 );
-  const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-  const torus = new THREE.Mesh( geometry, material );
-  scene.add( torus );
-  
-  // Camera
-  const camera = new THREE.PerspectiveCamera(
-      45, // Field of view, NB image easily distorted
-      800,600 // Aspect ratio
-  );
-  scene.add( camera );
-
-  // Renderer
-  const canvas = document.querySelector(".webgl");
-  const renderer = new THREE.WebGLRenderer({ canvas });
-  renderer.setSize(800,600)
-  renderer.render(scene, camera);
-
-
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <canvas class="webgl"></canvas>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
