dd3a4e5dc62eddce60ce7afe6feddcec88aed8e9
kurokaj <jomikuro@gmail.com>
2023-03-14 19:39:38 +0200

    Tutorial done. Next uploading own rigged model.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@
 
 import * as THREE from "three";
 import './style.css'
+import gsap from "gsap";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
  
 // Scene
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.SphereGeometry( 3, 64, 64 );
+const geometry = new THREE.SphereGeometry( 2, 64, 64 );
 const material = new THREE.MeshStandardMaterial( { color: 0x00ff83,  } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
@@ -24,11 +26,19 @@ const sizes = {
 // Light 
 const light = new THREE.PointLight(0xffffff, // Color of the light
                                     1,       //
+                                    100);    //
+
+const light2 = new THREE.PointLight(0xffffff, // Color of the light
+                                    1,       //
                                     100);    // 
 light.position.set(0,  // x
                    10, // y
                    10); // z
+light2.position.set(0,  // x
+                   -10, // y
+                   -10); // z
 scene.add( light );
+scene.add( light2 );
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -44,8 +54,16 @@ scene.add( camera );
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width,sizes.height)
+renderer.setPixelRatio(2) // more pixels -> smoother edges -> more calculations
 renderer.render(scene, camera);
 
+// Controls 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true; // slower movement
+controls.enablePan = false; // disable zooming and dragging
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
 
 // Resizer
 window.addEventListener("resize", ()=>{
@@ -60,12 +78,17 @@ window.addEventListener("resize", ()=>{
 
 // Loop
 const loop = () => {
+    controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(loop)
 };
 loop(); // rerenders every frame 
 
-
+// Animate the page (timeline magic)
+const tl = gsap.timeline({defaults:{duration:1}});
+tl.fromTo(sphere.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
+tl.fromTo('nav', {y:'-100%'}, {y:'0%'})
+tl.fromTo('.title', {opacity: '0'}, {opacity: '1'})
 
 //-------------------------------------------------
 //createApp(App).mount('#app')
