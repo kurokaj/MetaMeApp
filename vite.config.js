81c30b52054f53f377ad9dbbb2efa99e92f4c748
kurokaj <jomikuro@gmail.com>
2023-03-15 12:35:52 +0200

    wrong path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/buildvisionapp/'  // Only the main root
+  ? '/metameapp/'  // Only the main root
   : '/'
 })
