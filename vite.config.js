5d20f58e0aa66caa39228b72b3c1aca11db4384e
kurokaj <jomikuro@gmail.com>
2023-03-15 12:46:10 +0200

    new base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/metameapp/'  // Only the main root
-  : '/'
+  base: './'
 })
